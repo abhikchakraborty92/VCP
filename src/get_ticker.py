8412b752ab46f890dc37d2d0fa61f7da6b5a07c9
abhikchakraborty92 <abhik.chakraborty92@gmail.com>
2021-02-01 03:03:08 +0530

    Added google authentication to insert data to google sheets

--- src/get_ticker.py
+++ src/get_ticker.py
@@ -2,9 +2,9 @@ import pandas as pd
 from requests import get
 import json
 import datetime
-import pytz
-import sqlalchemy
+import hashlib
 import time
+from googleauthenticate import workbook
 
 # Reading configuration
 config = json.loads(open('config/config.json').read())
@@ -13,7 +13,29 @@ config = json.loads(open('config/config.json').read())
 tickerurl = config.get('market').get('ticker').get('url')
 tickercodes = config.get('tickercodes')
 
-while 1:
+# Getting the google sheet object into which the data has to be inserted
+rowlimit = config.get('googlesheetrowlimit')
+columns = config.get('googlesheetcolumns')
+
+def getworkbooksheet():
+    allsheets = workbook.fetch_sheet_metadata().get('sheets')
+    for sheetobj in allsheets:
+        sheet = workbook.get_worksheet(sheetobj.get('properties').get('index'))
+        try:
+            if len(sheet.col_values(1))+len(tickercodes)<=rowlimit:
+                return sheet
+            else:
+                 print(f'`{sheet.title}` sheet over limit. Ignoring...\n')
+
+        except:
+            return sheet
+    
+    print('Generating new worksheet...')
+    sheet = workbook.add_worksheet('New_'+datetime.datetime.now().strftime('%Y%m%d%H%M%S'),100,len(columns))
+    return sheet
+
+
+while True:
     # Getting ticker response
     refreshtimestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
     tickerresponse = get(tickerurl)
@@ -23,44 +45,61 @@ while 1:
         tickeroutput = json.loads(tickerresponse.text)
 
         for ticker in tickercodes:
-            tickerdatadict[ticker]=tickeroutput.get(ticker)
-
-
-    # Creating database connection and output dataset
-    cryptodb = sqlalchemy.create_engine('sqlite:///output/cryptoinfo.db')
+            tickerdatadict[ticker[0]]=tickeroutput.get(ticker[0])
 
     # Ticker data parsing functions
-    def tickerparse(output,tickercode):
+
+    # Generate unique key for each row
+    def generatekey(tickercode):
+        codestring = (tickercode+str(datetime.datetime.now())).encode()
+        return hashlib.md5(codestring).hexdigest()[:-10]
+
+    # Parse ticker response to data for the  tickers into rows
+    def tickerparse(output,tickercode,currname):
         try:
             parse = {
-                "tickercode" : [tickercode],
-                "base_unit" : [output.get('base_unit')],
-                "quote_unit" : [output.get('quote_unit')],
-                "low_price_24_hr" :[ output.get('low')],
-                "high_price_24_hr" : [output.get('high')],
-                "last_trade_price" : [output.get('last')],
-                "mkt_open_price" : [output.get('open')],
-                "trade_volume_24_hr" : [output.get('volume')],
-                "top_sell_price" : [output.get('sell')],
-                "top_buy_price" : [output.get('buy')],
-                "name" : [output.get('name')],
-                "ticker_timestamp" : [datetime.datetime.fromtimestamp(output.get('at')).strftime('%Y-%m-%d %H:%M')],
-                "refresh_timestamp" : [refreshtimestamp],
+                "insertid":str(generatekey(tickercode)),
+                "crypt_curr_name":currname.upper(),
+                "tickercode" : tickercode.upper(),
+                "base_unit" : output.get('base_unit'),
+                "quote_unit" : output.get('quote_unit'),
+                "low_price_24_hr" : float(output.get('low')),
+                "high_price_24_hr" : float(output.get('high')),
+                "last_trade_price" : float(output.get('last')),
+                "mkt_open_price" :float(output.get('open')),
+                "trade_volume_24_hr" : float(output.get('volume')),
+                "top_sell_price" : float(output.get('sell')),
+                "top_buy_price" : float(output.get('buy')),
+                "name" : output.get('name'),
+                "ticker_timestamp" : datetime.datetime.fromtimestamp(output.get('at')).strftime('%Y-%m-%d %H:%M'),
+                "refresh_timestamp" : refreshtimestamp,
                 "timezone": str(datetime.datetime.now().astimezone().tzinfo)
             }
-            ticker_dataset = pd.DataFrame(parse)
-            #print(datetime.datetime.fromtimestamp(output.get('at')))
-            ticker_dataset.to_sql('tickerinfo',cryptodb,if_exists='append',index=False)
-            return f"Ticker inserted for {tickercode} at {refreshtimestamp}"
+
+            # Inserting into google sheet
+            valuelist = list(parse.values())
+            return valuelist
         
-        except Exception as e:
+        except EnvironmentError as e:
             return e
 
         
 
     # Generating dataset for the tickers
-    for key,value in tickerdatadict.items():
-        print(tickerparse(value,key))
-    
-    time.sleep(60)
+    ValuesToInsert = []
+    for ticker in tickercodes:
+        ValuesToInsert.append(tickerparse(tickerdatadict.get(ticker[0]),ticker[0],ticker[1]))
 
+    sheet = getworkbooksheet()
+
+    try:
+        if sheet.cell(1,1).value == columns[0]:
+            pass
+        else:
+            sheet.insert_row(columns,1)
+    except Exception as e:
+        print(e)
+
+    sheet.append_rows(ValuesToInsert)
+
+    time.sleep(30)
---
+++ src/googleauthenticate.py
@@ -0,0 +1,24 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+
+config = json.loads(open('config/config.json').read())
+
+# Google API scopes
+scope = config.get('googlescope')
+
+# Creating Google Client
+try:
+    creds = ServiceAccountCredentials.from_json_keyfile_name("config/google_credentials.json", scope)
+    client = gspread.authorize(creds)
+    print('Authenticated')
+
+except:
+    print('Authentication Failed')
+# Getting the sheet where data has to be inserted
+try:
+    workbook = client.open(config.get('googleworkbookname'))
+    print("Workbook fetch successful")
+except Exception as e:
+    print(e)
+
